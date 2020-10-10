02c01f6c1a5b0edd00cce4ea9a1bb3392d8fbc7b
Lily <lilyau0112@gmail.com>
2020-10-11 00:13:38 +0800

    add active link

--- pages/item.js
+++ pages/item.js
@@ -25,7 +25,6 @@ export default async function Item(){
     ${hasComments ? story.comments.map(comment => 
         Comment(comment)).join('') : "No Comments"}
     </div>`
-
 }
 
 async function getStory(){
